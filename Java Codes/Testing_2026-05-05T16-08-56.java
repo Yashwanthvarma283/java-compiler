3c2cc49c4937707867ab36c73a2ba361f86b94ce
Yashwanthvarma283 <yashwanthvarmar@gmail.com>
2026-05-05 21:38:58 +0530

    Custom Code: Testing

---
+++ Java Codes/Testing_2026-05-05T16-08-56.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=nums/2;i++){
+            if(nums%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==nums ) return true;
+        else return false;
+    }
+}
