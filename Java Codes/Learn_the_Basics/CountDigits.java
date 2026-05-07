704584c623bc46b52987fbc032b806fb48fa24a3
Yashwanthvarma283 <yashwanthvarmar@gmail.com>
2026-05-07 12:09:38 +0530

    Solved: Count Digits

---
+++ Java Codes/Learn_the_Basics/CountDigits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countDigits(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+
+        return count;
+    }
+}
